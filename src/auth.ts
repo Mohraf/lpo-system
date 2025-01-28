db7a90c8a2db31c8a15b725b78ad5bb896ea2f10
mohraf <amosngok@gmail.com>
2025-01-28 08:18:05 +0300

    auth and middleware setup

---
+++ src/auth.ts
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaClient } from "@prisma/client";
+import { compare } from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email as string },
+        });
+
+        if (!user || !(await compare(credentials?.password as string, user.password))) {
+          return null;
+        }
+
+        return {
+          id: user.id.toString(),
+          name: `${user.firstName} ${user.lastName}`,
+          email: user.email,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.role = user.role;
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      session.user.role = token.role as string;
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
+});
