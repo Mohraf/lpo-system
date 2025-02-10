44e57b6a8159cb07fbc4c65f8f997a6c6dbbe53d
mohraf <amosngok@gmail.com>
2025-02-10 08:23:52 +0300

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-        <h1>Welcome to LPO SYSTEM</h1>
+    <div className="bg-gray-100 min-h-screen flex flex-col">
+      <div className="bg-white text-blue-600 h-16 px-6 flex items-center justify-start border-b-2 shadow-lg">
+        <h1 className="text-2xl font-bold">PURCHASE CHAIN</h1>
+      </div>
+
+      <div className="flex-auto flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center">
+          <h2 className="text-6xl font-semibold">Welcome to Purchase Chain</h2>
+          <h3 className="text-lg text-gray-700 mt-2">Simplify your procurement process today!</h3>
+        </div>
+
+        <div className="px-6 mt-6">
+          <Link
+            href="/login"
+            className="bg-blue-500 p-3 rounded-sm text-white hover:bg-blue-600 transition duration-300"
+          >
+            Sign in
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
